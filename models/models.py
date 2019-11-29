70ea12b3070fbe46b0e039522bd66d3ea3e93833
Levis Libra <levislibra@hotmail.com>
2019-11-29 17:19:18 -0300

    Update

--- models/models.py
+++ models/models.py
@@ -17,9 +17,15 @@ class FinancieraTablero(models.Model):
 	month = fields.Integer('Mes')
 	month_string = fields.Char('Mes')
 	year = fields.Integer('Año')
-	parent_id = fields.Many2one('financiera.tablero', 'Año o Mes')
-	month_ids = fields.One2many('financiera.tablero', 'parent_id', 'Meses', ondelete='cascade')
-	day_ids = fields.One2many('financiera.tablero', 'parent_id', 'Dias', ondelete='cascade')
+	prestamo_ids = fields.One2many('financiera.tablero.prestamo', 'tablero_id', 'Prestamos')
+	cuota_ids = fields.One2many('financiera.tablero.cuota', 'tablero_id', 'Cuotas')
+	parent_year_id = fields.Many2one('financiera.tablero', 'Año')
+	parent_month_id = fields.Many2one('financiera.tablero', 'Mes')
+	month_ids = fields.One2many('financiera.tablero', 'parent_year_id', 'Meses', ondelete='cascade')
+	day_ids = fields.One2many('financiera.tablero', 'parent_month_id', 'Dias', ondelete='cascade')
+	fecha_desde = fields.Date("Fecha desde")
+	fecha_hasta = fields.Date("Fecha hasta")
+	color = fields.Integer('Color Index')
 	company_id = fields.Many2one('res.company', 'Empresa', required=False, default=lambda self: self.env['res.company']._company_default_get('financiera.tablero'))
 
 	@api.model
@@ -40,46 +46,122 @@ class FinancieraTablero(models.Model):
 
 	@api.one
 	def button_crear_meses(self):
+		self.fecha_desde = str(self.year)+"-01-01"
+		self.fecha_hasta = str(self.year)+"-12-31"
 		i = 1
-		# print("Crear")
 		for month_string in MESES:
-			# print("Creando mes "+str(month_string))
+			dias_mes = monthrange(self.year, i)[1]
+			fecha_desde = str(self.year)+"-"+str(i).zfill(2)+"-01"
+			fecha_hasta = str(self.year)+"-"+str(i).zfill(2)+"-"+str(dias_mes).zfill(2)
 			t_values = {
 				'periodo': 'month',
 				'month': i,
 				'month_string': month_string,
 				'year': self.year,
-				'parent_id': self.id,
+				'parent_year_id': self.id,
+				'fecha_desde': fecha_desde,
+				'fecha_hasta': fecha_hasta,
 			}
 			tablero_mes_id = self.env['financiera.tablero'].create(t_values)
 			self.month_ids = [tablero_mes_id.id]
-			
 			# creamos los dias
-			dias_mes = monthrange(self.year, i)[1]
 			j = 1
-			# print("  dias del mes: "+str(dias_mes))
 			while j <= dias_mes:
-				# print("  Creando dia: "+str(j))
+				fecha_desde = str(self.year)+"-"+str(i).zfill(2)+"-"+str(j).zfill(2)
+				fecha_hasta = str(self.year)+"-"+str(i).zfill(2)+"-"+str(j).zfill(2)
 				t_values = {
 					'periodo': 'day',
 					'day': j,
 					'month': i,
 					'month_string': month_string,
 					'year': self.year,
-					'parent_id': tablero_mes_id.id,
+					'parent_month_id': tablero_mes_id.id,
+					'fecha_desde': fecha_desde,
+					'fecha_hasta': fecha_hasta,
 				}
 				tablero_dia_id = self.env['financiera.tablero'].create(t_values)
 				tablero_mes_id.day_ids = [tablero_dia_id.id]
 				j += 1
 			i += 1
 
+	@api.one
+	def actualizar_prestamos(self):
+		cr = self.env.cr
+		uid = self.env.uid
+		entidad_obj = self.pool.get('financiera.entidad')
+		entidad_ids = entidad_obj.search(cr, uid, [
+			('company_id', '=', self.company_id.id)])
+		for prestamo_id in self.prestamo_ids:
+			prestamo_id.unlink()
+		for _id in entidad_ids:
+			entidad_id = entidad_obj.browse(cr, uid, _id)
+			cantidad = 0
+			capital = 0
+			interes = 0
+			total = 0
+			meses_promedio = 0
+			nuevos = 0
+			prestamo_ids = []
+			if entidad_id.type == 'sucursal':
+				prestamo_obj = self.pool.get('financiera.prestamo')
+				prestamo_ids = prestamo_obj.search(cr, uid, [
+					('company_id', '=', self.company_id.id),
+					('fecha', '>=', self.fecha_desde),
+					('fecha', '<=', self.fecha_hasta),
+					('sucursal_id', '=', entidad_id.id)])
+			elif entidad_id.type == 'comercio':
+				prestamo_obj = self.pool.get('financiera.prestamo')
+				prestamo_ids = prestamo_obj.search(cr, uid, [
+					('company_id', '=', self.company_id.id),
+					('fecha', '>=', self.fecha_desde),
+					('fecha', '<=', self.fecha_hasta),
+					('comercio_id', '=', entidad_id.id)])
+			for _id in prestamo_ids:
+				prestamo_id = prestamo_obj.browse(cr, uid, _id)
+				cantidad += 1
+				capital += prestamo_id.monto_solicitado
+				interes += prestamo_id.interes_a_cobrar
+				total = capital+interes
+				prestamo_partner_obj = self.pool.get('financiera.prestamo')
+				prestamo_partner_ids = prestamo_partner_obj.search(cr, uid, [
+					('company_id', '=', self.company_id.id),
+					('partner_id', '=', prestamo_id.partner_id.id)])
+				if len(prestamo_partner_ids) == 1:
+					nuevos += 1
+			tp_values = {
+				'tablero_id': self.id,
+				'entidad_id': entidad_id.id,
+				'cantidad': cantidad,
+				'capital': capital,
+				'interes': interes,
+				'total': total,
+				'meses_promedio': meses_promedio,
+				'nuevos': nuevos,
+			}
+			tablero_prestamo_id = self.env['financiera.tablero.prestamo'].create(tp_values)
+			self.prestamo_ids = [tablero_prestamo_id.id]
+
+	def open_line(self, cr, uid, id, context=None):
+		return {
+			'type': 'ir.actions.act_window',
+			'name': 'Tablero', 
+			'view_type': 'form',
+			'view_mode': 'form',
+			'res_model': self._name,
+			'res_id': id[0],
+			'target': 'current',
+		}
+
 class FinancieraTableroPrestamo(models.Model):
 	_name = 'financiera.tablero.prestamo'
 
+	tablero_id = fields.Many2one('financiera.tablero', 'Tablero')
 	entidad_id = fields.Many2one('financiera.entidad', 'Entidad')
+	entidad_type = fields.Selection('Entidad tipo', related='entidad_id.type')
 	cantidad = fields.Integer('Cantidad')
 	capital = fields.Float('Capital', digits=(16,2))
 	interes = fields.Float('Interes', digits=(16,2))
+	total = fields.Float('Total', digits=(16,2))
 	meses_promedio = fields.Float('Meses promedio', digits=(16,2))
 	nuevos = fields.Integer('Nuevos')
 	company_id = fields.Many2one('res.company', 'Empresa', required=False, default=lambda self: self.env['res.company']._company_default_get('financiera.prestamo'))
@@ -87,7 +169,9 @@ class FinancieraTableroPrestamo(models.Model):
 class FinancieraTableroCuota(models.Model):
 	_name = 'financiera.tablero.cuota'
 
+	tablero_id = fields.Many2one('financiera.tablero', 'Tablero')
 	entidad_id = fields.Many2one('financiera.entidad', 'Entidad')
+	entidad_type = fields.Selection('Entidad tipo', related='entidad_id.type')
 	cantidad = fields.Integer('Cantidad')
 	capital = fields.Float('Capital', digits=(16,2))
 	interes = fields.Float('Interes', digits=(16,2))
